c3df2096ac67298371aaa2d9e7f1c5e644c4ab16
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-18 22:20:16 +0800

    add head and meta data on eom page

--- pages/eom.js
+++ pages/eom.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Navbar from "../components/Navbar";
 import styles from "../styles/eom.module.css";
 
@@ -17,18 +18,27 @@ export const getServerSideProps = async (pageContext) => {
 
 function Eom({ employee }) {
 	return (
-		<div className="page-container">
-			<Navbar />
-			<div className={styles.main}>
-				<h1>Employee of the Month</h1>
+		<>
+			<Head>
+				<title>Employee of the Month</title>
+				<meta
+					name="description"
+					content={`This month's employee of the month is ${employee.name}`}
+				/>
+			</Head>
+			<div className="page-container">
+				<Navbar />
+				<div className={styles.main}>
+					<h1>Employee of the Month</h1>
+				</div>
+				<div className={styles.eom}>
+					<h3>{employee.name}</h3>
+					<h6>{employee.position}</h6>
+					<img src={employee.image} />
+					<p>{employee.description}</p>
+				</div>
 			</div>
-			<div className={styles.eom}>
-				<h3>{employee.name}</h3>
-				<h6>{employee.position}</h6>
-				<img src={employee.image} />
-				<p>{employee.description}</p>
-			</div>
-		</div>
+		</>
 	);
 }
 
