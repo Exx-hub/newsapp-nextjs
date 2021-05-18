3b01abefb0a2dd67a07b8d15f292a2554976658e
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-18 14:53:22 +0800

    initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+	return (
+		<nav>
+			<h1>NEWS</h1>
+			<ul>
+				<li>HOME</li>
+				<li>DEV</li>
+				<li>SOCIAL</li>
+				<li>CONTACT</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Navbar;
---
+++ pages/dev.js
@@ -0,0 +1,11 @@
+function Dev() {
+	return (
+		<div className="page-container">
+			<div>
+				<h1>DEVELOPER</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Dev;
